69a2c40a831a2c4a2c554f3b5750d3a780a6d136
ClyingDeng <1174915260@qq.com>
2019-11-13 20:22:07 +0800

    二

---
+++ src/components/Showblog.vue
@@ -0,0 +1,58 @@
+<template>
+  <div v-theme="'wide'" class="showblog">
+    <h2>博客首页</h2>
+    <div v-for="blog in blogs" class="singleblog">
+      <h2 v-rainbow>{{blog.title}}</h2>
+      <article>{{blog.body}}</article>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      blogs: []
+    };
+  },
+  created() {
+    this.$http
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(function(data) {
+        // console.log(data)
+        this.blogs = data.body.slice(0, 10);
+        console.log(blogs);
+      });
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.showblog{
+  max-width: 800px;
+  margin: 0 auto;
+}
+.singleblog{
+  padding: 20px;
+  margin: 20px 0;
+  box-sizing: border-box;
+  background: #eee;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,25 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import VueResource from 'vue-resource'
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
+Vue.use(ElementUI);
+
+Vue.directive('rainbow', {
+  bind(el, binding, vnode) {
+    el.style.color = '#' + Math.random().toString(16).slice(2, 8)
+  }
+})
+Vue.directive('theme', {
+  bind(el, binding, vnode) {
+    if (binding.value == 'wide') {
+      el.style.maxWidth = '1260px'
+    } else if (binding.value == 'narrow') {
+      el.style.maxWidth = '560px'
+    }
+  }
+})
 
 Vue.use(VueResource)
 new Vue({
